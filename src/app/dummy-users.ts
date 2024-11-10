11f875877a022d2941bb8426f2129fa13b4e4dcb
Bahjat Khan <bahjat_khan2002@yahoo.com>
2024-11-09 19:38:47 -0500

    Warming up

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Tadim Johansberg',
+    avatar: 'user-1.jpg',
+  },
+  {
+    id: 'u2',
+    name: 'Tasha Nimandra',
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: 'u3',
+    name: 'Bushra Barns',
+    avatar: 'user-3.jpg',
+  },
+  {
+    id: 'u4',
+    name: 'Tasha Nimandra',
+    avatar: 'user-4.jpg',
+  },
+  {
+    id: 'u5',
+    name: 'Fhare Jacob',
+    avatar: 'user-5.jpg',
+  },
+  {
+    id: 'u6',
+    name: 'Maisha Alam',
+    avatar: 'user-6.jpg',
+  },
+];
---
+++ src/app/users/users.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+@Component({
+  selector: 'app-users',
+  standalone: true,
+  imports: [],
+  templateUrl: './users.component.html',
+  styleUrl: './users.component.css',
+})
+export class UsersComponent {
+  selectedUsers = DUMMY_USERS[0];
+}
